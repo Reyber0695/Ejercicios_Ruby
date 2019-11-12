8c1922b1cd7ee0cd1ea2578d699b3f24ec108dcc
trainee4 <dragonite0695@gmail.com>
2019-11-12 17:10:16 -0600

    Ejercio Corregido v2

--- Ejercicio_Ruby_3(Fechas).rb
+++ Ejercicio_Ruby_3(Fechas).rb
@@ -1,15 +1,15 @@
 require 'date'
 
-def dates(start_date, end_date, day)
+def getdates(start_date, end_date, day)
     diferences = end_date.month - start_date.month
     total_months = diferences + 1
-    date = []
+    dates = []
     month = 1
     while month <= total_months do
-      date.push(Date.new(start_date.year, month, day).strftime("%d %b %y")) 
+      dates.push(Date.new(start_date.year, month, day).strftime("%d %b %y")) 
       month += 1
     end
     date
 end
 
-dates(Date.new(2019,01,1), Date.new(2019,04,6), 3)
+getdates(Date.new(2019, 01, 1), Date.new(2019, 04, 6), 3)
