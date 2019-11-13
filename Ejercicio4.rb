27076e4368fa1e6997cfd822edcaa52952121b2f
trainee4 <dragonite0695@gmail.com>
2019-11-13 12:45:19 -0600

    Ejercio Corregido

--- Ejercicio4.rb
+++ Ejercicio4.rb
@@ -3,11 +3,8 @@ require 'stringio'
 
 def plusMinus(arr)
   positive = 0
-  positivepercent = 0
   negative = 0
-  negativepercent = 0
   zero = 0
-  zeropercent = 0
 
   arr.each do |number|
     positive += 1 if number.positive?()
@@ -15,13 +12,9 @@ def plusMinus(arr)
     zero += 1 if number.zero?()  
   end
   
-  positivepercent = positive.fdiv(arr.length)
-  negativepercent = negative.fdiv(arr.length)
-  zeropercent = zero.fdiv(arr.length)
-  
-  puts positivepercent
-  puts negativepercent
-  puts zeropercent
+  puts positive.fdiv(arr.length)
+  puts negative.fdiv(arr.length)
+  puts zero.fdiv(arr.length)
 end
 
 n = gets.to_i
