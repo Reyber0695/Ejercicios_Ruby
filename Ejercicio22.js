044b893829728d4768f339f79e8c59e1cae77e15
Reyber <reybersantos@outlook.es>
2019-11-23 10:34:05 -0600

    exercise 22 modified
    exercise 22 modified 2

--- Ejercicio22.js
+++ Ejercicio22.js
@@ -3,21 +3,13 @@ const word_finder = (term) => {
   const animals = ['Caballo', 'Oso', 'Perro'];
   const stuffs = ['Petroleo', 'Carne', 'Rosca'];
   let find_words = [];
-  personNames.forEach((trm)=>{
-    let exp = trm.toLowerCase().match(/.{1,2}/g);
-    if(exp.includes(term)) { find_words.push(trm); }
-  });
-  animals.forEach((trm)=>{
-    let exp = trm.toLowerCase().match(/.{1,2}/g);
-    if(exp.includes(term)) { find_words.push(trm); }
-  });
-  stuffs.forEach((trm)=>{
+
+  personNames.concat(animals, stuffs).forEach((trm)=>{
     let exp = trm.toLowerCase().match(/.{1,2}/g);
     if(exp.includes(term)) { find_words.push(trm); }
   });
 
-  console.log(find_words);
-    
+  return(find_words);
 }
   
-word_finder('pe');
+word_finder('ca');
